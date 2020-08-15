8f86a323bc9e0bc23078092dc15f05e4e49043d7
Kumar Shubham <shubhamstudent5@gmail.com>
2020-08-15 12:34:55 +0530

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r84=fd6s1hc8^44!31sm5q6rpmo^b-c1d*sm-&@b^gp^z6ww=%'
+SECRET_KEY = '**************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,14 +135,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shubhamstudent5@gmail.com'
-EMAIL_HOST_PASSWORD = 'ardazpkbdapmeclj'
+EMAIL_HOST_USER = '*****************'
+EMAIL_HOST_PASSWORD = '**************'
 
 DEFAULT_FILE_STORAGE = 'django_gcloud_storage.DjangoGCloudStorage'
 
-GCS_PROJECT = "ByteWalk"
-GCS_BUCKET = "bytewalk"
-GCS_CREDENTIALS_FILE_PATH = os.path.join(BASE_DIR, "my-key.json")
+GCS_PROJECT = "*********"
+GCS_BUCKET = "**********8"
+GCS_CREDENTIALS_FILE_PATH = os.path.join(BASE_DIR, "keyfile.json")
 GCS_USE_UNSIGNED_URLS = True
 
 
