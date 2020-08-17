d71357732c335aba47b501b191be2243c712eb52
Kumar Shubham <shubhamstudent5@gmail.com>
2020-08-17 19:27:45 +0530

    Add files via upload

--- feed/urls.py
+++ feed/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import PostUpdateView, PostDeleteView, PostListView, UserPostListView
 
@@ -7,7 +7,7 @@ urlpatterns=[
 	path('', PostListView.as_view(), name='home'),
 	path('post/new/', views.create_post, name='post-create'),
 	path('post/<int:pk>/', views.post_detail, name='post-detail'),
-	path('post/<int:pk>/like/', views.like, name='like'),
+	path('like/', views.like, name='post-like'),
 	path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 	path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 	path('search_posts/', views.search_posts, name='search_posts'),
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.urls import reverse
 from django.contrib import messages
 from django.core.paginator import Paginator
@@ -9,6 +9,8 @@ from django.views.generic import ListView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Comments, Like
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+import json
 
 class PostListView(ListView):
 	model = Post
@@ -16,22 +18,36 @@ class PostListView(ListView):
 	context_object_name = 'posts'
 	ordering = ['-date_posted']
 	paginate_by = 10
+	def get_context_data(self, **kwargs):
+		context = super(PostListView, self).get_context_data(**kwargs)
+		if self.request.user.is_authenticated:
+			liked = [i for i in Post.objects.all() if Like.objects.filter(user = self.request.user, post=i)]
+			context['liked_post'] = liked
+		return context
 
-class UserPostListView(ListView):
+class UserPostListView(LoginRequiredMixin, ListView):
 	model = Post
 	template_name = 'feed/user_posts.html'
 	context_object_name = 'posts'
 	paginate_by = 10
 
+	def get_context_data(self, **kwargs):
+		context = super(UserPostListView, self).get_context_data(**kwargs)
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		liked = [i for i in Post.objects.filter(user_name=user) if Like.objects.filter(user = self.request.user, post=i)]
+		context['liked_post'] = liked
+		return context
+
 	def get_queryset(self):
 		user = get_object_or_404(User, username=self.kwargs.get('username'))
 		return Post.objects.filter(user_name=user).order_by('-date_posted')
+
 
 @login_required
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	user = request.user
-	is_liked = Like.objects.filter(post=post, user=request.user).exists()
+	is_liked =  Like.objects.filter(user=user, post=post)
 	if request.method == 'POST':
 		form = NewCommentForm(request.POST)
 		if form.is_valid():
@@ -84,22 +100,38 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 			return True
 		return False
 
-@login_required
-def like(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-	new_like, created = Like.objects.get_or_create(user=request.user, post=post)
-	if not created:
-		new_like.delete()
-	return redirect('post-detail', pk=pk)
 
 @login_required
 def search_posts(request):
 	query = request.GET.get('p')
 	object_list = Post.objects.filter(tags__icontains=query)
+	liked = [i for i in object_list if Like.objects.filter(user = request.user, post=i)]
 	context ={
-		'posts': object_list
+		'posts': object_list,
+		'liked_post': liked
 	}
 	return render(request, "feed/search_posts.html", context)
+
+@login_required
+def like(request):
+	post_id = request.GET.get("likeId", "")
+	user = request.user
+	post = Post.objects.get(pk=post_id)
+	liked= False
+	like = Like.objects.filter(user=user, post=post)
+	if like:
+		like.delete()
+	else:
+		liked = True
+		Like.objects.create(user=user, post=post)
+	resp = {
+        'liked':liked
+    }
+	response = json.dumps(resp)
+	return HttpResponse(response, content_type = "application/json")
+
+
+
 
 
 
