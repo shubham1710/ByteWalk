0ff1538dfbe3927799a7275caa87e700e1ff1c1d
Kumar Shubham <shubhamstudent5@gmail.com>
2021-01-02 08:44:32 +0530

    Changed user list

--- users/views.py
+++ users/views.py
@@ -14,37 +14,37 @@ User = get_user_model()
 
 @login_required
 def users_list(request):
-	users = Profile.objects.exclude(user=request.user)
-	sent_friend_requests = FriendRequest.objects.filter(from_user=request.user)
-	sent_to = []
-	friends = []
-	for user in users:
-		friend = user.friends.all()
-		for f in friend:
-			if f in friends:
-				friend = friend.exclude(user=f.user)
-		friends+=friend
-	my_friends = request.user.profile.friends.all()
-	for i in my_friends:
-		if i in friends:
-			friends.remove(i)
-	if request.user.profile in friends:
-		friends.remove(request.user.profile)
-	random_list = random.sample(list(users), min(len(list(users)), 10))
-	for r in random_list:
-		if r in friends:
-			random_list.remove(r)
-	friends+=random_list
-	for i in my_friends:
-		if i in friends:
-			friends.remove(i)
-	for se in sent_friend_requests:
-		sent_to.append(se.to_user)
-	context = {
-		'users': friends,
-		'sent': sent_to
-	}
-	return render(request, "users/users_list.html", context)
+    users = Profile.objects.exclude(user=request.user)
+    sent_friend_requests = FriendRequest.objects.filter(from_user=request.user)
+    my_friends = request.user.profile.friends.all()
+    sent_to = []
+    friends = []
+    for user in my_friends:
+        friend = user.friends.all()
+        for f in friend:
+            if f in friends:
+                friend = friend.exclude(user=f.user)
+        friends += friend
+    for i in my_friends:
+        if i in friends:
+            friends.remove(i)
+    if request.user.profile in friends:
+        friends.remove(request.user.profile)
+    random_list = random.sample(list(users), min(len(list(users)), 10))
+    for r in random_list:
+        if r in friends:
+            random_list.remove(r)
+    friends += random_list
+    for i in my_friends:
+        if i in friends:
+            friends.remove(i)
+    for se in sent_friend_requests:
+        sent_to.append(se.to_user)
+    context = {
+        'users': friends,
+        'sent': sent_to
+    }
+    return render(request, "users/users_list.html", context)
 
 def friend_list(request):
 	p = request.user.profile
