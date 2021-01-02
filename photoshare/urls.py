0b620bb43dcd012c18180328bca85eff36ff282a
Kumar Shubham <shubhamstudent5@gmail.com>
2021-01-02 08:06:23 +0530

    Update urls.py

--- photoshare/urls.py
+++ photoshare/urls.py
@@ -20,12 +20,12 @@ urlpatterns = [
 	path('my-profile/', user_views.my_profile, name='my_profile'),
 	path('search_users/', user_views.search_users, name='search_users'),
 	path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+        path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+        path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+        path('password-reset/done/', auth_views.PasswordResetView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+        path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+        path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
