2d708f038c43712e9e64dacd5d53d49704fb6ebd
Kumar Shubham <shubhamstudent5@gmail.com>
2021-10-01 08:38:40 +0530

    Update models.py

--- feed/models.py
+++ feed/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
 
+# This model is for any post that a user posts on the website.
 class Post(models.Model):
 	description = models.CharField(max_length=255, blank=True)
 	pic = models.ImageField(upload_to='path/to/img')
@@ -17,13 +18,14 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
  
-
+# Comment model links a comment with the post and the user. 
 class Comments(models.Model):
 	post = models.ForeignKey(Post, related_name='details', on_delete=models.CASCADE)
 	username = models.ForeignKey(User, related_name='details', on_delete=models.CASCADE)
 	comment = models.CharField(max_length=255)
 	comment_date = models.DateTimeField(default=timezone.now)
 
+# It stores the like info. It has the user who created the like and the post on which like was made.
 class Like(models.Model):
 	user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
 	post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)	
